20bae934c12caba5a9aca8f164bb8ccef4e2f6fd
Richard Nyström <ricn@users.noreply.github.com>
2017-09-23 17:38:47 +0200

    Add support to set html title

--- lib/rapport.ex
+++ lib/rapport.ex
@@ -6,7 +6,7 @@ defmodule Rapport do
 
   @base_template File.read!(Path.join(__DIR__, "base_template.html.eex"))
 
-  defstruct template: nil, paper_size: nil, rotation: nil, fields: nil
+  defstruct template: nil, paper_size: nil, rotation: nil, fields: nil, title: nil
 
   def new(template_path, paper_size \\ :A4, rotation \\ :portrait) do
     {:ok, template_content} = File.read(template_path)
@@ -14,10 +14,15 @@ defmodule Rapport do
       template: template_content,
       paper_size: paper_size,
       rotation: rotation,
-      fields: %{}}
+      fields: %{},
+      title: "Report"}
   end
 
-  def set_field(%Rapport{} = report, field_name, field_value) do
+  def set_field(%Rapport{} = report, field_name, field_value) when is_binary(field_name) do
+    set_field(report, String.to_atom(field_name), field_value)
+  end
+
+  def set_field(%Rapport{} = report, field_name, field_value) when is_atom(field_name) do
     fields =
       report.fields
       |> Map.put(field_name, field_value)
@@ -25,8 +30,10 @@ defmodule Rapport do
     Map.put(report, :fields, fields)
   end
 
+  def set_title(%Rapport{} = report, title), do: Map.put(report, :title, title)
+
   def generate_html(%Rapport{} = report) do
     content = EEx.eval_string report.template, assigns: report.fields
-    EEx.eval_string @base_template, assigns: [content: content]
+    EEx.eval_string @base_template, assigns: [content: content, title: report.title]
   end
 end
--- test/rapport_test.exs
+++ test/rapport_test.exs
@@ -7,20 +7,19 @@ defmodule RapportTest do
   @list_template Path.join(__DIR__, "templates/list.html.eex")
   @list_map_template Path.join(__DIR__, "templates/list_map.html.eex")
 
-  test "hello world" do
+  ### set_field
+  test "set_field" do
     html_report =
       Rapport.new(@hello_template)
       |> Rapport.set_field(:hello, "Hello World!")
       |> Rapport.generate_html
 
     assert html_report =~ "Hello World!"
-    assert html_report =~ "@page { size: A4 }"
-    assert html_report =~ "body class=\"A4\""
   end
 
-  test "set two fields" do
+  test "set_field two times" do
     html_report =
-      Rapport.new(@two_fields_template, :A4)
+      Rapport.new(@two_fields_template)
       |> Rapport.set_field(:first, "first!")
       |> Rapport.set_field(:second, "second!")
       |> Rapport.generate_html
@@ -29,9 +28,9 @@ defmodule RapportTest do
     assert html_report =~ "second!"
   end
 
-  test "set a field with list" do
+  test "set_field with a list" do
     html_report =
-      Rapport.new(@list_template, :A4)
+      Rapport.new(@list_template)
       |> Rapport.set_field(:list, ["one", "two", "three"])
       |> Rapport.generate_html
 
@@ -40,19 +39,39 @@ defmodule RapportTest do
     assert html_report =~ "three"
   end
 
-  test "field with a list of maps" do
+  test "set_field with a list of maps" do
     people = [
       %{firstname: "Richard", lastname: "Nyström", age: 33},
       %{firstname: "Kristin", lastname: "Nyvall", age: 34},
       %{firstname: "Nils", lastname: "Nyvall", age: 3}
     ]
     html_report =
-      Rapport.new(@list_map_template, :A4)
+      Rapport.new(@list_map_template)
       |> Rapport.set_field(:people, people)
       |> Rapport.generate_html
 
     assert html_report =~ "Richard"
     assert html_report =~ "Nyvall"
     assert html_report =~ "33"
+  end
+
+  test "set_field using a string key" do
+    html_report =
+      Rapport.new(@hello_template)
+      |> Rapport.set_field("hello", "Hello World!")
+      |> Rapport.generate_html
+
+    assert html_report =~ "Hello World!"
+  end
+
+  ### set_title
+  test "set_title" do
+    html_report =
+      Rapport.new(@hello_template)
+      |> Rapport.set_title("My title")
+      |> Rapport.set_field("hello", "Hello World!")
+      |> Rapport.generate_html
+
+    assert html_report =~ "<title>My title</title>"
   end
 end
