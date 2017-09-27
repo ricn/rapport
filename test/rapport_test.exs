6b68ca5375369468952f2e62edc31071754086a5
Richard Nyström <ricn@users.noreply.github.com>
2017-09-27 21:08:36 +0200

    Test clean up

--- test/rapport_test.exs
+++ test/rapport_test.exs
@@ -10,7 +10,7 @@ defmodule RapportTest do
 
   describe "new" do
 
-    test "simplest possible" do
+    test "must set sane defaults" do
       report = Rapport.new
       assert is_map(report)
       assert Map.has_key?(report, :title)
@@ -21,7 +21,7 @@ defmodule RapportTest do
       assert is_list(Map.get(report, :pages))
     end
 
-    test "set title" do
+    test "must set title" do
       report = Rapport.new("My report")
       assert Map.get(report, :title) == "My report"
     end
@@ -38,6 +38,7 @@ defmodule RapportTest do
       end
     end
 
+    # Move this to add_page
     test "must allow inline template" do
       inline_template = """
       <section class="sheet padding-10mm">
@@ -52,148 +53,11 @@ defmodule RapportTest do
       assert html_report =~ "Inline template"
     end
 
-    test "A4 portrait" do
-      html_report =
-        Rapport.new(@empty_template, :A4, :portrait)
-        |> Rapport.generate_html
-
-      assert html_report =~ "<style>@page {size: A4}</style>"
-      assert html_report =~ "<body class=\"A4\">"
-    end
-
-    test "A4 landscape" do
-      html_report =
-        Rapport.new(@empty_template, :A4, :landscape)
-        |> Rapport.generate_html
-
-      assert html_report =~ "<style>@page {size: A4 landscape}</style>"
-      assert html_report =~ "<body class=\"A4 landscape\">"
-    end
-
-    test "A3 portrait" do
-      html_report =
-        Rapport.new(@empty_template, :A3, :portrait)
-        |> Rapport.generate_html
-
-      assert html_report =~ "<style>@page {size: A3}</style>"
-      assert html_report =~ "<body class=\"A3\">"
-    end
-
-    test "A3 landscape" do
-      html_report =
-        Rapport.new(@empty_template, :A3, :landscape)
-        |> Rapport.generate_html
-
-      assert html_report =~ "<style>@page {size: A3 landscape}</style>"
-      assert html_report =~ "<body class=\"A3 landscape\">"
-    end
-
-    test "A5 portrait" do
-      html_report =
-        Rapport.new(@empty_template, :A5, :portrait)
-        |> Rapport.generate_html
-
-      assert html_report =~ "<style>@page {size: A5}</style>"
-      assert html_report =~ "<body class=\"A5\">"
-    end
-
-    test "A5 landscape" do
-      html_report =
-        Rapport.new(@empty_template, :A5, :landscape)
-        |> Rapport.generate_html
-
-      assert html_report =~ "<style>@page {size: A5 landscape}</style>"
-      assert html_report =~ "<body class=\"A5 landscape\">"
-    end
-
-    test "half_letter portrait" do
-      html_report =
-        Rapport.new(@empty_template, :half_letter, :portrait)
-        |> Rapport.generate_html
-
-      assert html_report =~ "<style>@page {size: half_letter}</style>"
-      assert html_report =~ "<body class=\"half_letter\">"
-    end
-
-    test "half_letter landscape" do
-      html_report =
-        Rapport.new(@empty_template, :half_letter, :landscape)
-        |> Rapport.generate_html
-
-      assert html_report =~ "<style>@page {size: half_letter landscape}</style>"
-      assert html_report =~ "<body class=\"half_letter landscape\">"
-    end
-
-    test "letter portrait" do
-      html_report =
-        Rapport.new(@empty_template, :letter, :portrait)
-        |> Rapport.generate_html
-
-      assert html_report =~ "<style>@page {size: letter}</style>"
-      assert html_report =~ "<body class=\"letter\">"
-    end
-
-    test "letter landscape" do
-      html_report =
-        Rapport.new(@empty_template, :letter, :landscape)
-        |> Rapport.generate_html
-
-      assert html_report =~ "<style>@page {size: letter landscape}</style>"
-      assert html_report =~ "<body class=\"letter landscape\">"
-    end
-
-    test "legal portrait" do
-      html_report =
-        Rapport.new(@empty_template, :legal, :portrait)
-        |> Rapport.generate_html
-
-      assert html_report =~ "<style>@page {size: legal}</style>"
-      assert html_report =~ "<body class=\"legal\">"
-    end
-
-    test "legal landscape" do
-      html_report =
-        Rapport.new(@empty_template, :legal, :landscape)
-        |> Rapport.generate_html
-
-      assert html_report =~ "<style>@page {size: legal landscape}</style>"
-      assert html_report =~ "<body class=\"legal landscape\">"
-    end
-
-    test "junior legal portrait" do
-      html_report =
-        Rapport.new(@empty_template, :junior_legal, :portrait)
-        |> Rapport.generate_html
-
-      assert html_report =~ "<style>@page {size: junior_legal}</style>"
-      assert html_report =~ "<body class=\"junior_legal\">"
-    end
-
-    test "junior legal landscape" do
-      html_report =
-        Rapport.new(@empty_template, :junior_legal, :landscape)
-        |> Rapport.generate_html
-
-      assert html_report =~ "<style>@page {size: junior_legal landscape}</style>"
-      assert html_report =~ "<body class=\"junior_legal landscape\">"
-    end
-
-    test "ledger portrait" do
-      html_report =
-        Rapport.new(@empty_template, :ledger, :portrait)
-        |> Rapport.generate_html
-
-      assert html_report =~ "<style>@page {size: ledger}</style>"
-      assert html_report =~ "<body class=\"ledger\">"
-    end
-
-    test "ledger landscape" do
-      html_report =
-        Rapport.new(@empty_template, :ledger, :landscape)
-        |> Rapport.generate_html
-
-      assert html_report =~ "<style>@page {size: ledger landscape}</style>"
-      assert html_report =~ "<body class=\"ledger landscape\">"
+    test "all allowed paper sizes" do
+      all = [:A4, :A3, :A5, :half_letter, :letter, :legal, :junior_legal, :ledger]
+      Enum.each(all, fn(paper_size) ->
+        assert Rapport.new("Title", paper_size, :portrait).paper_size == paper_size
+      end)
     end
   end
 
@@ -204,6 +68,7 @@ defmodule RapportTest do
         |> Rapport.add_page(@hello_template, %{hello: "Hello world"})
 
       assert length(report.pages) == 1
+      assert List.first(report.pages).fields.hello == "Hello world"
     end
 
     test "add two pages" do
@@ -216,15 +81,40 @@ defmodule RapportTest do
     end
   end
 
-  ### set_field
-  describe "set_field/2" do
+  describe "generate_html" do
+    test "must include normalize & paper css" do
+      html_report =
+        Rapport.new
+        |> Rapport.generate_html
+      assert html_report =~ "normalize.css v7.0.0"
+      assert html_report =~ "paper.css"
+    end
+
+    test "must set paper size correctly" do
+      html_report =
+        Rapport.new("Report", :A5, :portrait)
+        |> Rapport.generate_html
+
+      assert html_report =~ "<style>@page {size: A5}</style>"
+      assert html_report =~ "<body class=\"A5\">"
+    end
+
+    test "must set rotation correctly" do
+      html_report =
+        Rapport.new("Report", :A5, :landscape)
+        |> Rapport.generate_html
+
+      assert html_report =~ "<style>@page {size: A5 landscape}</style>"
+      assert html_report =~ "<body class=\"A5 landscape\">"
+    end
+
     test "set_field" do
       html_report =
         Rapport.new
         |> Rapport.add_page(@hello_template, %{hello: "Hello World!"})
         |> Rapport.generate_html
 
-      assert html_report =~ "Hello World!"
+      assert html_report =~ "<article>Hello World!</article>"
     end
 
     test "set_field two times" do
@@ -267,15 +157,6 @@ defmodule RapportTest do
       assert html_report =~ "Nyvall"
       assert html_report =~ "33"
     end
-  end
 
-  describe "generate_html" do
-    test "make sure normalize & paper css is included" do
-      html_report =
-        Rapport.new
-        |> Rapport.generate_html
-      assert html_report =~ "normalize.css v7.0.0"
-      assert html_report =~ "paper.css"
-    end
   end
 end
