1905b7bfcc387b10fc5f02dfced9104a2c080f96
Richard Nyström <ricn@users.noreply.github.com>
2017-09-29 10:17:01 +0200

    ex_doc should be used in dev

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Rapport.Mixfile do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:excoveralls, "~> 0.7", only: [:dev, :test]},
-      {:ex_doc, ">= 0.0.0", only: :docs},
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:inch_ex, only: :docs}
     ]
   end
