cea63492ab28914e8668fbfa88470146ea8d69d2
Richard Nyström <ricn@users.noreply.github.com>
2017-10-08 22:26:19 +0200

    Refactor

--- lib/page.ex
+++ lib/page.ex
@@ -1,3 +1,94 @@
 defmodule Rapport.Page do
   defstruct template: nil, fields: nil
+
+  alias Rapport.Report
+  alias Rapport.Page
+
+  @doc """
+  Adds a new page to a report.
+
+  ## Options
+
+    * `report` - A `Rapport.Report` struct that you want to add the page to.
+    * `page_template` - An EEx template for the page
+    * `fields` - A map with fields that must be assigned to the EEx template
+  """
+
+  def add_page(%Report{} = report, page_template, %{} = fields) when is_binary(page_template) do
+    new_page = %Page{template: page_template, fields: fields}
+    Map.put(report, :pages, [new_page | report.pages])
+  end
+
+  @doc """
+  Adds a new page to a report.
+
+  ## Options
+
+    * `report` - A `Rapport.Report` struct that you want to add the page to.
+    * `page` - A `Rapport.Page` struct
+  """
+  def add_page(%Report{} = report, %Page{} = page) do
+    Map.put(report, :pages, [page | report.pages])
+  end
+
+  @doc """
+  Adds a list of pages to a report.
+
+  ## Options
+    * `report` - A `Rapport.Report` struct that you want to add the page to.
+    * `pages` - A list with `Rapport.Page` structs
+  """
+  def add_pages(%Report{} = report, pages) when is_list(pages) do
+    Map.put(report, :pages, pages ++ report.pages)
+  end
+
+  @doc false
+  def generate_pages(pages, padding) when is_list(pages) do
+    Enum.reverse(pages)
+    |> Enum.map(fn(page) -> generate_page(page, padding) end)
+    |> Enum.join
+  end
+
+  @doc false
+  def generate_pages(pages, padding, add_page_numbers, page_number_position, page_number_formatter) when is_list(pages) do
+    total_pages = Enum.count(pages)
+    Enum.reverse(pages)
+    |> Enum.with_index
+    |> Enum.map(fn({page, index}) -> generate_page(page, padding, index + 1, total_pages, page_number_position, page_number_formatter) end)
+    |> Enum.join
+  end
+
+  @doc false
+  def generate_page(p, padding) do
+    EEx.eval_string wrap_page_with_padding(p.template, padding), assigns: p.fields
+  end
+
+  @doc false
+  def generate_page(p, padding, page_number, total_pages, page_number_position, page_number_formatter) do
+    EEx.eval_string wrap_page_with_padding(p.template, padding, page_number, total_pages, page_number_position, page_number_formatter), assigns: p.fields
+  end
+
+  @doc false
+  def wrap_page_with_padding(template, padding) do
+    padding_css = "padding-" <> Integer.to_string(padding) <> "mm"
+    """
+    <div class=\"sheet #{padding_css}\">
+      #{template}
+    </div>
+    """
+  end
+
+  @doc false
+  def wrap_page_with_padding(template, padding, page_number, total_pages, page_number_position, page_number_formatter) do
+    padding_css = "padding-" <> Integer.to_string(padding) <> "mm"
+    position = Atom.to_string(page_number_position)
+    page_number_tag = "<span class='page-numbering #{position}'>#{page_number_formatter.(page_number, total_pages)}</span>"
+    """
+    <div class=\"sheet #{padding_css}\">
+      #{template}
+      #{page_number_tag}
+    </div>
+    """
+  end
+
 end
--- lib/rapport.ex
+++ lib/rapport.ex
@@ -12,6 +12,18 @@ defmodule Rapport do
   @paper_css File.read!(Path.join(__DIR__, "paper.css"))
   @base_template File.read!(Path.join(__DIR__, "base_template.html.eex"))
 
+  defdelegate add_page(report, page_template, fields), to: Rapport.Page
+  defdelegate add_page(report, page), to: Rapport.Page
+  defdelegate add_pages(report, pages), to: Rapport.Page
+
+  defdelegate generate_pages(pages, padding), to: Rapport.Page
+  defdelegate generate_pages(pages, padding, add_page_numbers, page_number_position, page_number_formatter), to: Rapport.Page
+
+  defdelegate generate_page(p, padding), to: Rapport.Page
+  defdelegate generate_page(p, padding, page_number, total_pages, page_number_position, page_number_formatter), to: Rapport.Page
+  defdelegate wrap_page_with_padding(template, padding), to: Rapport.Page
+  defdelegate wrap_page_with_padding(template, padding, page_number, total_pages, page_number_position, page_number_formatter), to: Rapport.Page
+
   @doc """
   Creates a new report.
 
@@ -45,44 +57,6 @@ defmodule Rapport do
       page_number_position: :bottom_left,
       page_number_formatter: fn(cnt_page, _tot_pages) -> "#{cnt_page}" end
     }
-  end
-
-  @doc """
-  Adds a new page to a report.
-
-  ## Options
-
-    * `report` - A `Rapport.Report` struct that you want to add the page to.
-    * `page_template` - An EEx template for the page
-    * `fields` - A map with fields that must be assigned to the EEx template
-  """
-
-  def add_page(%Report{} = report, page_template, %{} = fields) when is_binary(page_template) do
-    new_page = %Page{template: page_template, fields: fields}
-    Map.put(report, :pages, [new_page | report.pages])
-  end
-
-  @doc """
-  Adds a new page to a report.
-
-  ## Options
-
-    * `report` - A `Rapport.Report` struct that you want to add the page to.
-    * `page` - A `Rapport.Page` struct
-  """
-  def add_page(%Report{} = report, %Page{} = page) do
-    Map.put(report, :pages, [page | report.pages])
-  end
-
-  @doc """
-  Adds a list of pages to a report.
-
-  ## Options
-    * `report` - A `Rapport.Report` struct that you want to add the page to.
-    * `pages` - A list with `Rapport.Page` structs
-  """
-  def add_pages(%Report{} = report, pages) when is_list(pages) do
-    Map.put(report, :pages, pages ++ report.pages)
   end
 
   @doc """
@@ -194,55 +168,6 @@ defmodule Rapport do
     |> Map.put(:add_page_numbers, true)
     |> Map.put(:page_number_position, page_number_position)
     |> Map.put(:page_number_formatter, formatter)
-  end
-
-  #########################
-  ### Private functions ###
-  #########################
-
-  defp generate_pages(pages, padding) when is_list(pages) do
-    Enum.reverse(pages)
-    |> Enum.map(fn(page) -> generate_page(page, padding) end)
-    |> Enum.join
-  end
-
-  defp generate_pages(pages, padding, add_page_numbers, page_number_position, page_number_formatter) when is_list(pages) do
-    total_pages = Enum.count(pages)
-    Enum.reverse(pages)
-    |> Enum.with_index
-    |> Enum.map(fn({page, index}) -> generate_page(page, padding, index + 1, total_pages, page_number_position, page_number_formatter) end)
-    |> Enum.join
-  end
-
-  defp generate_page(p, padding) do
-    EEx.eval_string wrap_page_with_padding(p.template, padding), assigns: p.fields
-  end
-
-  defp generate_page(p, padding, page_number, total_pages, page_number_position, page_number_formatter) do
-    EEx.eval_string wrap_page_with_padding(p.template, padding, page_number, total_pages, page_number_position, page_number_formatter), assigns: p.fields
-  end
-
-  # TODO: Change name
-  defp wrap_page_with_padding(template, padding) do
-    padding_css = "padding-" <> Integer.to_string(padding) <> "mm"
-    """
-    <div class=\"sheet #{padding_css}\">
-      #{template}
-    </div>
-    """
-  end
-
-  # TODO: Change name
-  defp wrap_page_with_padding(template, padding, page_number, total_pages, page_number_position, page_number_formatter) do
-    padding_css = "padding-" <> Integer.to_string(padding) <> "mm"
-    position = Atom.to_string(page_number_position)
-    page_number_tag = "<span class='page-numbering #{position}'>#{page_number_formatter.(page_number, total_pages)}</span>"
-    """
-    <div class=\"sheet #{padding_css}\">
-      #{template}
-      #{page_number_tag}
-    </div>
-    """
   end
 
   defp paper_settings_css(%Report{} = report) do
