762e539cc6bf7eba09a42b76609223283638e548
Richard Nyström <ricn@users.noreply.github.com>
2017-10-09 16:41:26 +0200

    Refactor

--- lib/rapport/barcode.ex
+++ lib/rapport/barcode.ex
@@ -10,26 +10,28 @@ defmodule Rapport.Barcode do
   end
 
   defp create_code39(text) do
-    out_file = Path.join([System.tmp_dir!, "barcode_#{UUID.uuid4()}.png"])
+    out_file = generate_random_file()
     Barlix.Code39.encode!(text) |> Barlix.PNG.print(file: out_file)
     File.read!(out_file)
   end
 
   defp create_code93(text) do
-    out_file = Path.join([System.tmp_dir!, "barcode_#{UUID.uuid4()}.png"])
+    out_file = generate_random_file()
     Barlix.Code93.encode!(text) |> Barlix.PNG.print(file: out_file)
     File.read!(out_file)
   end
 
   defp create_code128(text) do
-    out_file = Path.join([System.tmp_dir!, "barcode_#{UUID.uuid4()}.png"])
+    out_file = generate_random_file()
     Barlix.Code128.encode!(text) |> Barlix.PNG.print(file: out_file)
     File.read!(out_file)
   end
 
   defp create_itf(text) do
-    out_file = Path.join([System.tmp_dir!, "barcode_#{UUID.uuid4()}.png"])
+    out_file = generate_random_file()
     Barlix.Code128.encode!(text) |> Barlix.PNG.print(file: out_file)
     File.read!(out_file)
   end
+
+  defp generate_random_file, do: Path.join([System.tmp_dir!, "barcode_#{UUID.uuid4()}.png"])
 end
