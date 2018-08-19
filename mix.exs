69a6a17f581f1b71fdee05252b363648677c6a4d
Richard Nyström <ricn@rny.io>
2018-08-19 21:38:20 +0200

    Upgrade deps

--- mix.exs
+++ mix.exs
@@ -37,10 +37,10 @@ defmodule Rapport.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:excoveralls, "~> 0.8", only: [:dev, :test]},
-      {:ex_doc, "0.18.1", only: :dev},
-      {:inch_ex, "0.5.6", only: :docs},
-      {:faker, "0.9.0", only: :test},
+      {:excoveralls, "0.9.2", only: [:dev, :test]},
+      {:ex_doc, "0.19.1", only: :dev},
+      {:inch_ex, "1.0.0", only: :docs},
+      {:faker, "0.10.0", only: :test},
       {:barlix, "0.3.3"},
       {:uuid, "1.1.8"}
     ]
