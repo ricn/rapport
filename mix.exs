131c3eb6d276ff73c36654867bbb1ef7e67fcec0
Richard Nyström <ricn@rny.io>
2021-11-28 00:21:49 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rapport.Mixfile do
   def project do
     [
       app: :rapport,
-      version: "0.6.6",
+      version: "0.6.7",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -35,8 +35,6 @@ defmodule Rapport.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       {:excoveralls, "0.14.4", only: [:dev, :test]},
       {:ex_doc, "0.26.0", only: :dev},
       {:inch_ex, "2.0.0", only: :docs},
