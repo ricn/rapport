694342dc3987d04314280a091ece639de195cee7
Richard Nyström <ricn@rny.io>
2021-12-09 23:07:39 +0100

    Add typespec for Report struct.

--- lib/rapport/report.ex
+++ lib/rapport/report.ex
@@ -7,4 +7,15 @@ defmodule Rapport.Report do
             padding: nil,
             fields: nil,
             page_number_opts: nil
+
+  @type t :: %Rapport.Report{
+          paper_size: :A4 | :A3 | :A5 | :half_letter | :letter | :legal | :junior_legal,
+          rotation: :portrait | :landscape,
+          title: String.t(),
+          pages: list(Rapport.Page),
+          template: String.t(),
+          padding: non_neg_integer(),
+          fields: map(),
+          page_number_opts: %Rapport.PageNumbering{}
+        }
 end
