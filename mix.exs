b5ebb91f3c41800de66f6a5e121d0f8c4637e747
Richard Nyström <ricn@rny.io>
2024-04-25 22:11:38 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rapport.Mixfile do
   def project do
     [
       app: :rapport,
-      version: "0.7.1",
+      version: "0.7.2",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
